8a491993df589d8d30fb3ad4846f1ffb3ba99ea0
Asahina <r.asahina@ks-japan.jp>
2020-06-29 15:56:56 +0900

    検索機能修正

---
+++ src/jp/co/aforce/beans/SearchProductBean.java
@@ -0,0 +1,65 @@
+/*
+ * ユーザが検索した際に選択したカテゴリと商品名の一時格納
+ */
+package jp.co.aforce.beans;
+
+public class SearchProductBean{
+	private String category;
+	private String id;
+	private String name;
+	private String image;
+	private int price;
+	private String detail;
+
+
+	public String getCategory() {
+		return category;
+	}
+
+	public void setCategory(String category) {
+		this.category = category;
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getImage() {
+		return image;
+	}
+
+	public void setImage(String image) {
+		this.image = image;
+	}
+
+	public int getPrice() {
+		return price;
+	}
+
+	public void setPrice(int price) {
+		this.price = price;
+	}
+
+	public String getDetail() {
+		return detail;
+	}
+
+	public void setDetail(String detail) {
+		this.detail = detail;
+	}
+
+
+
+}
